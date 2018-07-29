a3abe010d5f2ac595645843f0aebe742ba3c2fc0
Yannis <standinthesun@sina.com>
2018-07-29 19:08:07 +0800

    Finish Task3

--- main/main.js
+++ main/main.js
@@ -1,3 +1,7 @@
-module.exports = function main() {
-  // Write your code here
+module.exports = function main(strr) {
+  summ = 0;
+    for (var i=0; i<strr.length; i++){
+      summ += Number(strr[i]);
+    }
+    return summ;
 };
